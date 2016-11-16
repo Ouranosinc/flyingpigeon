6d7dab279fc1f83cfde64070664bb88cbdbb433f
David Huard <david.huard@gmail.com>
2016-11-16 15:36:05 -0500

    Start with netCDF, thinking going to ocgis.

--- flyingpigeon/processes/wps_spatial_analog.py
+++ flyingpigeon/processes/wps_spatial_analog.py
@@ -7,6 +7,13 @@ Author: Nils Hempelmann (info@nilshempelmann.de), David Huard (huard.david@ouran
 
 from pywps.Process import WPSProcess
 import logging
+from os.path import basename
+import numpy as np
+from flyingpigeon import dist_diff as dd
+# from flyingpigeon.utils import archive
+import json, datetime as dt
+import netCDF4 as nc
+
 logger = logging.getLogger(__name__)
 
 from flyingpigeon.sdm import _SDMINDICES_
@@ -31,70 +38,76 @@ class SDMProcess(WPSProcess):
 
         # Literal Input Data
         # ------------------
-        
-        
-        
-        self.resources = self.addComplexInput(
-            identifier="resources",
-            title="Reference NetCDF File",
-            abstract="Reference NetCDF file storing the variables required for the computation of the indices.",
+
+        self.reference_nc = self.addComplexInput(
+            identifier="reference_nc",
+            title="Reference NetCDF Dataset",
+            abstract="NetCDF dataset storing the reference indices.",
             minOccurs=1,
-            maxOccurs=500,
+            maxOccurs=10,
             maxmegabites=50000,
             formats=[{"mimeType":"application/x-netcdf"}],
             )
 
-        self.resources = self.addComplexInput(
-            identifier="resources",
-            title="Target NetCDF File",
-            abstract="Target NetCDF file storing the variables required for the computation of the indices at the target site over the target period.",
-            minOccurs=1,
-            maxOccurs=500,
+        self.target_nc = self.addComplexInput(
+            identifier="target_nc",
+            title="Target NetCDF dataset",
+            abstract="Target NetCDF dataset storing the target indices over the target period.",
+            minOccurs=0,
+            maxOccurs=10,
             maxmegabites=50000,
             formats=[{"mimeType":"application/x-netcdf"}],
             )
-        # self.BBox = self.addBBoxInput(
-        #     identifier="BBox",
-        #     title="Bounding Box",
-        #     abstract="coordinates to define the region to be analysed",
-        #     minOccurs=1,
-        #     maxOccurs=1,
-        #     crss=['EPSG:4326']
-        #     )
 
+        self.target_json = self.addComplexInput(
+            identifier="target_json",
+            title="Target JSON dataset",
+            abstract="Target JSON dataset storing the target indices over the target period in a dictionary keyed by variable names.",
+            minOccurs=0,
+            maxOccurs=10,
+            maxmegabites=50000,
+            formats=[{"mimeType": "application/json"}],
+        )
 
-        self.coords = self.addLiteralInput(
-          identifier="coords",
-          title="Target site coordinates",
-          abstract="Comma separated tuple of WGS85 lon,lat decimal coordinates.",
-          default="2.356138, 48.846450",
-          type=type(''),
-          minOccurs=1,
-          maxOccurs=1,
-          )
-
-        self.input_indices = self.addLiteralInput(
-            identifier="input_indices",
-            title="Indices at the target site.",
-            abstract="One or more climate indices to compute over the reference and target data.",
-            default=['TG_JJA', 'TNn_Jan'],
+        self.indices = self.addLiteralInput(
+            identifier="indices",
+            title="Indices",
+            abstract="One or more climate indices to use for the comparison.",
             type=type(''),
             minOccurs=1,
             maxOccurs=10,
-            allowedValues=_SDMINDICES_ 
             )
 
-
-        self.period = self.addLiteralInput(
-            identifier="period",
-            title="Reference period",
-            abstract="Reference period for climate conditions (all = entire timeseries)",
-            default="all",
+        self.algo = self.addLiteralInput(
+            identifier="algo",
+            title="Algorithm",
+            abstract="Name of algorithm to use to compute differences.",
+            default="kldiv",
+            allowedValues=['kldiv', ],
             type=type(''),
-            minOccurs=1,
+            minOccurs=0,
             maxOccurs=1,
-            allowedValues=['all','1951-1980', '1961-1990', '1971-2000','1981-2010']
+        )
+
+        self.refrange = self.addLiteralInput(
+            identifier="refrange",
+            title="Reference period",
+            abstract="Reference period (YYYY-MM-DD/YYYY-MM-DD) for climate conditions (defaults to entire timeseries).",
+            default="",
+            type=type(''),
+            minOccurs=0,
+            maxOccurs=1,
             )
+
+        self.targetrange = self.addLiteralInput(
+            identifier="targetrange",
+            title="Target period",
+            abstract="Target periods (YYYY-MM-DD/YYYY-MM-DD) for climate conditions (defaults to entire timeseries). Only applies to netCDF target files.",
+            default="",
+            type=type(''),
+            minOccurs=0,
+            maxOccurs=20,
+        )
 
         self.archive_format = self.addLiteralInput(
             identifier="archive_format",
@@ -102,184 +115,109 @@ class SDMProcess(WPSProcess):
             abstract="Result files will be compressed into archives. Choose an appropriate format.",
             default="tar",
             type=type(''),
-            minOccurs=1,
+            minOccurs=0,
             maxOccurs=1,
             allowedValues=['zip','tar']
             )
 
-        
         ###########
         ### OUTPUTS
         ###########
-             
-        self.output_indices = self.addComplexOutput(
-            identifier="output_indices",
-            title="Climate indices over all timesteps (3D)",
-            abstract="Archive (tar/zip) containing calculated climate indices as netCDF files",
-            formats=[{"mimeType":"application/x-tar"}, {"mimeType":"application/zip"}],
-            asReference=True,
-            )         
-         
-        self.output_analogs = self.addComplexOutput(
-            identifier="output_analogs",
+        #self.output = self.addComplexOutput(
+        #    identifier="output_analogs",
+        #    title="Spatial Analogs",
+        #    abstract="Archive (tar/zip) containing calculated dissimilarity measure as netCDF files",
+        #    formats=[{"mimeType":"application/x-tar"}, {"mimeType":"application/zip"}],
+        #    asReference=True,
+        #    )
+        self.output = self.addLiteralOutput(
+            identifier="output_test",
             title="Spatial Analogs",
-            abstract="Archive (tar/zip) containing calculated climate indices as netCDF files",
-            formats=[{"mimeType":"application/x-tar"}, {"mimeType":"application/zip"}],
-            asReference=True,
-            )
-
-        self.output_example = self.addComplexOutput(
-            identifier="output_example",
-            title="Example Climate Analog",
-            abstract="an example netCDF file picked from the archive to be displayed on WMS",
-            formats=[{"mimeType":"application/x-netcdf"}],
-            asReference=True,
-            )
-
-        self.output_info = self.addComplexOutput(
-            identifier="output_info",
-            title="GAM statistics information",
-            abstract="Graphics and information of the learning statistics",
-            formats=[{"mimeType":"image/png"}],
-            asReference=True,
-            )
+            type = type(1.0),
+        )
 
     def execute(self):
-      from os.path import basename
-      from flyingpigeon import sdm
-      from flyingpigeon import spatial_analog as sa
-      from flyingpigeon.utils import archive
 
-      self.status.set('Start process', 0)
-      
-      try: 
-        logger.info('reading the arguments')
-        resources = self.getInputValues(identifier='resources')
-        #taxon_name = self.getInputValues(identifier='taxon_name')[0]
-        #period = self.period.getValue()
-        coords = self.getInputValues(identifier='coords')[0]
-        period = self.getInputValues(identifier='period')[0]
-        coordinate = [float(n) for n in coords.split(',')]
-        
-        #indices = self.input_indices.getValue()
-        indices = self.getInputValues(identifier='input_indices')
-        logger.info("indices = %s ", indices)
-        
-        archive_format = self.archive_format.getValue()
-      except Exception as e: 
-        logger.error('failed to read in the arguments %s ' % e)
-     
-      #################################
-      ### calculate the climate indices
-      #################################
-      
-      # get the indices
-      ncs_indices = None
-      try:
-        self.status.set('start calculation of climate indices for %s' % indices, 30 )
-        ncs_indices = sdm.get_indices(resources=resources, indices=indices)
-        logger.info('indice calculation done')
-      except:
-        msg = 'failed to calculate indices'
-        logger.debug(msg)
-        # raise Exception(msg)
-
-      try:
-        archive_indices = archive(ncs_indices , format=archive_format)
-        logger.info('indices 3D added to tarfile')
-      except:
-        msg = 'failed adding indices to tar'  
-        logger.debug(msg)
-        # raise Exception(msg)  
-
-      indices_dic = None
-      try: 
-        # sort indices
-        indices_dic = sdm.sort_indices(ncs_indices)
-        logger.info('indice files sorted for %s datasets' % len(indices_dic.keys()))
-      except:
-        msg = 'failed to sort indices'
-        logger.debug(msg)
-        # raise Exception(msg)
-
-      ncs_references = []
-      analogs = []
-      statistics_info = []
-
-      for count, key in enumerate(indices_dic.keys()):
-        try:
-          self.status.set('Start processing of %s ' % key, 40 + count * 10)
-          ncs = indices_dic[key]
-          logger.info('with %s files' % len(ncs))
-
-          gam_model, statistic_plot = sa.get_gam(ncs, coordinate)
-          statistics_info.append(statistic_plot)
-          self.status.set('GAM sucessfully trained', 70)
-        except:
-          msg = 'failed to train GAM'  
-          logger.debug(msg)
-          # raise Exception(msg)
+        self.status.set('Start process', 0)
 
         try:
-          prediction = sdm.get_prediction(gam_model, ncs_indices)
-          self.status.set('prediction done', 80)
-        except:
-          msg = 'failed to predict'   
-          logger.debug(msg)
-          # raise Exception(msg)
-          
-      #   try:
-      #     from numpy import invert, isnan, nan, broadcast_arrays, array, zeros, linspace, meshgrid
-      #     mask = invert(isnan(PAmask))
-      #     mask = broadcast_arrays(prediction, mask)[1]
-      #     prediction[mask==False] = nan
-      #     self.status.set('land sea mask for predicted data', 90)
-      #   except: 
-      #     logger.debug('failed to mask predicted data')
+            logger.info('reading the arguments')
+            reference_nc = self.getInputValues(identifier='reference_nc')
+            target_nc = self.getInputValues(identifier='target_nc')
+            target_json = self.getInputValues(identifier='target_json')
+            indices = self.getInputValues(identifier='indices')
+            algo = self.getInputValue(identifier='algo')
+            refrange = self.getInputValue(identifier='refrange')
+            targetrange = self.getInputValues(identifier='targetrange')
+            archive_fmt = self.getInputValue(identifier='archive_format')
 
-        try: 
-          analogs.append(sdm.write_to_file(ncs_indices[0], prediction))
+        except Exception as e:
+            logger.error('failed to read in the arguments %s ' % e)
 
-          logger.info('Analog written to file')
-          #tar_prediction.add(species_file, 
-           #               arcname = basename(species_file))#.replace(os.path.abspath(os.path.curdir), ""))
-        except:
-          msg = 'failed to write species file'
-          logger.debug(msg)
-          # raise Exception(msg)
+        if target_nc is None or target_json is None:
+            raise ValueError("Target data is missing.")
 
-      from flyingpigeon.visualisation import concat_images
-      statistics_infos = None
-      try: 
-        statistics_infos = concat_images(statistics_info, orientation='v')
-        logger.info('statistc graphics concatinated')
-      except:
-        msg = 'failed to concat images'  
-        logger.debug(msg)
-        # raise Exception(msg)  
+        # Load target variables
+        target_data = {}
+        if target_json is not None:
+            for target in target_json:
+                with open(target, 'r') as f:
+                    data = json.load(f)
+                    for var in indices:
+                        target_data[var] = data[var]
 
-      # # archive_references = None
-      # # try:
-      # #   archive_references = archive(ncs_references , format=archive_format)
-      # #   logger.info('indices 2D added to archive')
-      # # except:
-      # #   msg = 'failed adding 2D indices to archive'  
-      # #   logger.debug(msg)
-      # #   # raise Exception(msg) 
-      # archive_analogs = None
-      
-      try:
-        archive_analogs = archive(analogs , format=archive_format)
-        logger.info('analog file added to archive')
-      except:
-        msg = 'failed adding analog file to archive'  
-        logger.debug(msg)
-        # raise Exception(msg)  
+        elif target_nc is not None:
+            with nc.MFDataset(target_nc, 'r') as D:
+                for var in indices:
+                    #TODO : Implement targetrange
+                    target_data[var] = D.variables[var][:]
 
-      self.output_indices.setValue( archive_indices )
-      self.output_analogs.setValue( archive_analogs )
-      i = next((i for i, x in enumerate(analogs) if x), None)
-      self.output_example.setValue (analogs[i])
-      self.output_info.setValue(statistics_infos)
+        P = np.vstack(target_data[var] for var in indices).T
 
-      self.status.set('done', 100)
+        # Load reference dataset
+        ptime = lambda x: dt.datetime.strptime(x, "%Y-%m-%d")
+        if refrange:
+            refrange = map(ptime, refrange.split('-'))
+
+
+        ref_data = {}
+        with nc.MFDataset(reference_nc, 'r') as D:
+            time = D.variables['time']
+            if refrange:
+                sl = slice(*nc.date2index(rrange, time, select='nearest'))
+            else:
+                sl = slice(None, None)
+
+            for var in indices:
+                assert D.variables[var].dimensions[0] == 'time'
+                ref_data[var] = D.variables[var][sl]
+
+            shape = set([ref_data[var].shape for var in indices])
+            assert len(shape) == 1
+            shape = shape.pop()
+
+            out_arr = np.ones(shape[1:]) * -999
+            I,J = np.indices(shape[1:])
+            # TODO: parallelize
+            for (i,j) in zip(I.flatten(),J.flatten()):
+                Q = np.vstack([ref_data[var][:,i,j] for var in indices]).T
+                if np.any(np.isnan(Q)):
+                    continue
+
+                if algo == 'kldiv':
+                    out_arr[i,j] = dd.kldiv(P, Q)
+                else:
+                    raise NotImplementedError(algo)
+
+            fn = 'output.nc'
+            with nc.Dataset(fn, 'w') as D:
+                D.setncattr('source', 'spatial_analog')
+                #TODO: Copy dimensions and attributes
+
+            D.createDimension('lat', )
+            D.createVariable(algo, float, ('lat', 'lon'))
+
+        #self.output.setValue(fn)
+
+        self.output.setValue(ans)
+        self.status.set('done', 100)
